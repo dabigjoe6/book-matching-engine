368166ddf920818d0594ffee0761e8816b739ff9
Joseph Olabisi <dabigjoe6@gmail.com>
2024-11-08 02:11:09 +0000

    Add gitignore

---
+++ CMakeFiles/LimitOrderBook_lib.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LimitOrderBook_lib.dir/src/Limit/limit.cpp.o"
+  "CMakeFiles/LimitOrderBook_lib.dir/src/Limit/limit.cpp.o.d"
+  "CMakeFiles/LimitOrderBook_lib.dir/src/Order/order.cpp.o"
+  "CMakeFiles/LimitOrderBook_lib.dir/src/Order/order.cpp.o.d"
+  "CMakeFiles/LimitOrderBook_lib.dir/src/OrderBook/order_book.cpp.o"
+  "CMakeFiles/LimitOrderBook_lib.dir/src/OrderBook/order_book.cpp.o.d"
+  "libLimitOrderBook_lib.a"
+  "libLimitOrderBook_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LimitOrderBook_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
