3a43089540954e2be99bc844de4f7439dd543193
Joseph Olabisi <dabigjoe6@gmail.com>
2025-05-17 21:39:40 +0100

    update gitignore

--- test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mac/Documents/c++/limitorderbook/test
-# Build directory: /Users/mac/Documents/c++/limitorderbook/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[LimitOrderBookTest]=] "/Users/mac/Documents/c++/limitorderbook/test/LimitOrderBookTest")
-set_tests_properties([=[LimitOrderBookTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/mac/Documents/c++/limitorderbook/test/CMakeLists.txt;12;add_test;/Users/mac/Documents/c++/limitorderbook/test/CMakeLists.txt;0;")
