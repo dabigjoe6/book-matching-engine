18ba4e876b4ffa3eead27c0cb404ba29dfc57c78
Joseph Olabisi <dabigjoe6@gmail.com>
2025-07-27 20:09:28 +0100

    add test for deleting node left and right children

--- test/AvlTreeTest.cpp
+++ test/AvlTreeTest.cpp
@@ -255,3 +255,77 @@ TEST_F(AvlTreeTests, AvlTreeDeletingNodeWithOnlyRightChild) {
   EXPECT_EQ(tree->get_root()->get_value(), 8);
   EXPECT_EQ(tree->get_root()->get_right_child()->get_value(), 12);
 }
+
+TEST_F(AvlTreeTests, AvlTreeDeletingNodeWithLeftAndRightChild) {
+  /*
+    *                    7                                           7
+    *                  /   \            Deleting node 12           /   \
+    *                 4      12             ------>               4      13
+  *                 /  \    /   \                               /  \    /  \
+  *                 1  5   9      14                           1   5   9   14
+  *                      / \     /  \                                 / \     \
+  *                     8   11  13  15                               8  11     15
+    */
+
+  LimitType limitType = LimitType::LimitBuy;
+
+  Limit* nodeA = new Limit(12, limitType);
+  Limit* nodeB = new Limit(7, limitType);
+  Limit* nodeC = new Limit(4, limitType);
+  Limit* nodeD = new Limit(9, limitType);
+  Limit* nodeE = new Limit(1, limitType);
+  Limit* nodeF = new Limit(5, limitType);
+  Limit* nodeG = new Limit(8, limitType);
+  Limit* nodeH = new Limit(11, limitType);
+
+  Limit* nodeI = new Limit(14, limitType);
+  Limit* nodeJ = new Limit(13, limitType);
+  Limit* nodeK = new Limit(15, limitType);
+
+  AvlTree<Limit>* tree = new AvlTree<Limit>(nodeA);
+
+  tree->insert_node(nodeB);
+  tree->insert_node(nodeI);
+  tree->insert_node(nodeC);
+  tree->insert_node(nodeD);
+  tree->insert_node(nodeE);
+  tree->insert_node(nodeF);
+  tree->insert_node(nodeG);
+  tree->insert_node(nodeH);
+  tree->insert_node(nodeJ);
+  tree->insert_node(nodeK);
+
+  EXPECT_EQ(tree->get_root()->get_value(), 7);
+  EXPECT_EQ(tree->get_root()->get_left_child()->get_value(), 4);
+  EXPECT_EQ(tree->get_root()->get_right_child()->get_value(), 12);
+
+  EXPECT_EQ(tree->get_root()->get_left_child()->get_right_child()->get_value(), 5);
+  EXPECT_EQ(tree->get_root()->get_left_child()->get_left_child()->get_value(), 1);
+
+  EXPECT_EQ(tree->get_root()->get_right_child()->get_right_child()->get_value(), 14);
+  EXPECT_EQ(tree->get_root()->get_right_child()->get_left_child()->get_value(), 9);
+
+  EXPECT_EQ(tree->get_root()->get_right_child()->get_right_child()->get_left_child()->get_value(), 13);
+  EXPECT_EQ(tree->get_root()->get_right_child()->get_right_child()->get_right_child()->get_value(), 15);
+
+  EXPECT_EQ(tree->get_root()->get_right_child()->get_left_child()->get_left_child()->get_value(), 8);
+  EXPECT_EQ(tree->get_root()->get_right_child()->get_left_child()->get_right_child()->get_value(), 11);
+
+  tree->delete_node(nodeA);
+
+  EXPECT_EQ(tree->get_root()->get_value(), 7);
+  EXPECT_EQ(tree->get_root()->get_left_child()->get_value(), 4);
+  EXPECT_EQ(tree->get_root()->get_right_child()->get_value(), 13);
+
+  EXPECT_EQ(tree->get_root()->get_left_child()->get_right_child()->get_value(), 5);
+  EXPECT_EQ(tree->get_root()->get_left_child()->get_left_child()->get_value(), 1);
+
+  EXPECT_EQ(tree->get_root()->get_right_child()->get_right_child()->get_value(), 14);
+  EXPECT_EQ(tree->get_root()->get_right_child()->get_left_child()->get_value(), 9);
+
+  EXPECT_EQ(tree->get_root()->get_right_child()->get_right_child()->get_right_child()->get_value(), 15);
+
+  EXPECT_EQ(tree->get_root()->get_right_child()->get_left_child()->get_left_child()->get_value(), 8);
+  EXPECT_EQ(tree->get_root()->get_right_child()->get_left_child()->get_right_child()->get_value(), 11);
+}
+
